562919538198a93d09df8a4e6e69c317887d0bd1
정다한 <jeongdahan@jeongdahan-ui-MacBookPro.local>
2024-02-24 10:56:20 +0900

    <feat>: career page 임시 저장

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Text } from '@styles/components';
 import { useFunnel } from 'hooks/useFunnel';
 
 const CareerPage = () => {
@@ -13,6 +14,7 @@ const CareerPage = () => {
       <Funnel>
         <Funnel.Step name="직무">
           <h1 onClick={() => setStep('연차')}>직무</h1>
+          <Job />
         </Funnel.Step>
         <Funnel.Step name="연차">
           <h2 onClick={() => setStep('관심사')}>연차</h2>
@@ -21,6 +23,16 @@ const CareerPage = () => {
           <h4 onClick={() => console.log('완료')}>관심사</h4>
         </Funnel.Step>
       </Funnel>
+    </div>
+  );
+};
+
+const Job = () => {
+  return (
+    <div>
+      <Text typo="display_1" color="gray_90">
+        하시는 일을 알려주세요.
+      </Text>
     </div>
   );
 };
